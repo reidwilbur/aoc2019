97ead4265ba3f4e9539cfe9af6af77f930947647
Reid Wilbur <reid.wilbur@gmail.com>
2019-12-03 11:19:23 -0600

    Day3 sln2

--- src/main/java/com/wilb0t/aoc/Day3.java
+++ src/main/java/com/wilb0t/aoc/Day3.java
@@ -1,12 +1,20 @@
 package com.wilb0t.aoc;
 
 import com.google.common.collect.Sets;
-import java.util.AbstractMap;
+import io.norberg.automatter.AutoMatter;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Day3 {
+
+  @AutoMatter
+  public interface Point {
+    int x();
+
+    int y();
+  }
 
   static int getClosestIntersection(String wirePath1, String wirePath2) {
     var wirePoints1 = toPoints(wirePath1);
@@ -15,15 +23,28 @@ public class Day3 {
     var crosses = Sets.intersection(wirePoints1, wirePoints2);
 
     return crosses.stream()
-        .map(point -> Math.abs(point.getKey()) + Math.abs(point.getValue()))
+        .map(point -> Math.abs(point.x()) + Math.abs(point.y()))
         .sorted()
         .findFirst()
         .get();
   }
 
-  static Set<Map.Entry<Integer, Integer>> toPoints(String path) {
-    Map.Entry<Integer, Integer> location = new AbstractMap.SimpleEntry<>(0, 0);
-    var points = new HashSet<Map.Entry<Integer, Integer>>();
+  static int getShortestPathIntersection(String wirePath1, String wirePath2) {
+    var wirePoints1 = toPointsWithPathLen(wirePath1);
+    var wirePoints2 = toPointsWithPathLen(wirePath2);
+
+    var crosses = Sets.intersection(wirePoints1.keySet(), wirePoints2.keySet());
+
+    return crosses.stream()
+        .map(p -> wirePoints1.get(p) + wirePoints2.get(p))
+        .sorted()
+        .findFirst()
+        .get();
+  }
+
+  static Set<Point> toPoints(String path) {
+    var location = new PointBuilder().x(0).y(0).build();
+    var points = new HashSet<Point>();
     for (var seg : path.split(",")) {
       var dir = seg.charAt(0);
       var len = Integer.parseInt(seg.substring(1));
@@ -35,16 +56,33 @@ public class Day3 {
     return points;
   }
 
-  static Map.Entry<Integer, Integer> updateLocation(
-      Map.Entry<Integer, Integer> location, char dir) {
+  static Map<Point, Integer> toPointsWithPathLen(String path) {
+    var location = new PointBuilder().x(0).y(0).build();
+    var points = new HashMap<Point, Integer>();
+    var pathLen = 0;
+    for (var seg : path.split(",")) {
+      var dir = seg.charAt(0);
+      var len = Integer.parseInt(seg.substring(1));
+      for (int l = 0; l < len; l++) {
+        location = updateLocation(location, dir);
+        pathLen += 1;
+        if (!points.containsKey(location)) {
+          points.put(location, pathLen);
+        }
+      }
+    }
+    return points;
+  }
+
+  static Point updateLocation(Point location, char dir) {
     if (dir == 'U') {
-      return new AbstractMap.SimpleEntry<>(location.getKey(), location.getValue() + 1);
+      return PointBuilder.from(location).y(location.y() + 1).build();
     } else if (dir == 'D') {
-      return new AbstractMap.SimpleEntry<>(location.getKey(), location.getValue() - 1);
+      return PointBuilder.from(location).y(location.y() - 1).build();
     } else if (dir == 'L') {
-      return new AbstractMap.SimpleEntry<>(location.getKey() - 1, location.getValue());
+      return PointBuilder.from(location).x(location.x() - 1).build();
     } else if (dir == 'R') {
-      return new AbstractMap.SimpleEntry<>(location.getKey() + 1, location.getValue());
+      return PointBuilder.from(location).x(location.x() + 1).build();
     }
     throw new RuntimeException("Unknown direction " + dir);
   }
--- src/test/java/com/wilb0t/aoc/Day3Test.java
+++ src/test/java/com/wilb0t/aoc/Day3Test.java
@@ -49,4 +49,23 @@ public class Day3Test {
   void testGetClosestIntersection_part1() {
     assertThat(Day3.getClosestIntersection(path1, path2), is(721));
   }
+
+  @Test
+  void testGetShortestPathIntersection_case1() {
+    var path1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+    var path2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+    assertThat(Day3.getShortestPathIntersection(path1, path2), is(610));
+  }
+
+  @Test
+  void testGetShortestPathIntersection_case2() {
+    var path1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+    var path2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+    assertThat(Day3.getShortestPathIntersection(path1, path2), is(410));
+  }
+
+  @Test
+  void testGetShortestPathIntersection_part2() {
+    assertThat(Day3.getShortestPathIntersection(path1, path2), is(7388));
+  }
 }
