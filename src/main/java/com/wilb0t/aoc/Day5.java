b862720c7a9116a6f2a2156817015e1c6fd0f7ee
Reid Wilbur <reid.wilbur@gmail.com>
2019-12-05 13:02:03 -0600

    Day5 sln2

--- src/main/java/com/wilb0t/aoc/Day5.java
+++ src/main/java/com/wilb0t/aoc/Day5.java
@@ -5,68 +5,107 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Day5 {
+
+  static class Instr {
+
+    private final String instr;
+    private final boolean imm1;
+    private final boolean imm2;
+    private final boolean imm3;
+    private final int ip;
+    private final List<Integer> code;
+
+    Instr(int ip, List<Integer> code) {
+      instr = String.format("%05d", code.get(ip));
+      imm3 = instr.charAt(0) == '1';
+      imm2 = instr.charAt(1) == '1';
+      imm1 = instr.charAt(2) == '1';
+      this.ip = ip;
+      this.code = code;
+    }
+
+    String opcode() {
+      return instr.substring(3);
+    }
+
+    Integer p1() {
+      return code.get(ip + 1);
+    }
+
+    Integer p2() {
+      return code.get(ip + 2);
+    }
+
+    Integer p3() {
+      return code.get(ip + 3);
+    }
+
+    Integer in1() {
+      var param = code.get(ip + 1);
+      return imm1 ? param : code.get(param);
+    }
+
+    Integer in2() {
+      var param = code.get(ip + 2);
+      return imm2 ? param : code.get(param);
+    }
+
+    Integer in3() {
+      var param = code.get(ip + 3);
+      return imm3 ? param : code.get(param);
+    }
+  }
+
   static class Computer {
     static final String ADD = "01";
     static final String MUL = "02";
     static final String IN = "03";
     static final String OUT = "04";
+    static final String JIT = "05";
+    static final String JIF = "06";
+    static final String LT = "07";
+    static final String EQ = "08";
     static final int HLT = 99;
 
     public List<Integer> exec(List<Integer> code, Iterator<Integer> input) {
       var ip = 0;
       var output = new ArrayList<Integer>();
       while (code.get(ip) != HLT) {
-        var instr = String.format("%05d", code.get(ip));
-        var opCode = instr.substring(3);
-        var im3 = instr.charAt(0) == '1';
-        var im2 = instr.charAt(1) == '1';
-        var im1 = instr.charAt(2) == '1';
+        var instr = new Instr(ip, code);
 
-        switch (opCode) {
+        switch (instr.opcode()) {
           case ADD:
-            {
-              var p1 = code.get(ip + 1);
-              var p2 = code.get(ip + 2);
-              var p3 = code.get(ip + 3);
-
-              var in1 = im1 ? p1 : code.get(p1);
-              var in2 = im2 ? p2 : code.get(p2);
-
-              code.set(p3, in1 + in2);
-              ip += 4;
-              break;
-            }
+            code.set(instr.p3(), instr.in1() + instr.in2());
+            ip += 4;
+            break;
           case MUL:
-            {
-              var p1 = code.get(ip + 1);
-              var p2 = code.get(ip + 2);
-              var p3 = code.get(ip + 3);
-
-              var in1 = im1 ? p1 : code.get(p1);
-              var in2 = im2 ? p2 : code.get(p2);
-
-              p3 = code.get(ip + 3);
-              code.set(p3, in1 * in2);
-              ip += 4;
-              break;
-            }
-          case IN: {
-            var p1 = code.get(ip + 1);
-
-            code.set(p1, input.next());
+            code.set(instr.p3(), instr.in1() * instr.in2());
+            ip += 4;
+            break;
+          case IN:
+            code.set(instr.p1(), input.next());
             ip += 2;
             break;
-          }
-          case OUT: {
-            var p1 = code.get(ip + 1);
-            var in1 = im1 ? p1 : code.get(p1);
-
-            output.add(in1);
+          case OUT:
+            output.add(instr.in1());
             ip += 2;
             break;
-          }
+          case JIT:
+            ip = instr.in1() != 0 ? instr.in2() : ip + 3;
+            break;
+          case JIF:
+            ip = instr.in1() == 0 ? instr.in2() : ip + 3;
+            break;
+          case LT:
+            code.set(instr.p3(), instr.in1() < instr.in2() ? 1 : 0);
+            ip += 4;
+            break;
+          case EQ:
+            code.set(instr.p3(), instr.in1().equals(instr.in2()) ? 1 : 0);
+            ip += 4;
+            break;
           default:
-            throw new IllegalArgumentException("Invalid opcode " + opCode + " at ip " + ip);
+            throw new IllegalArgumentException("Invalid opcode " + instr.opcode() + " at ip " + ip);
         }
       }
       return output;
--- src/test/java/com/wilb0t/aoc/Day5Test.java
+++ src/test/java/com/wilb0t/aoc/Day5Test.java
@@ -9,7 +9,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -60,9 +59,71 @@ public class Day5Test {
 
   @Test
   void testPart1() {
-    var localCode = new ArrayList<Integer>(code);
+    var localCode = new ArrayList<>(code);
     var output = computer.exec(localCode, List.of(1).iterator());
     assertTrue(output.subList(0, output.size() - 1).stream().allMatch(code -> code.equals(0)));
     assertThat(output.get(output.size() - 1), is(4601506));
+  }
+
+  @Test
+  void testExec_part2_case1() {
+    var code = Lists.newArrayList(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(0)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(8).iterator()), is(List.of(1)));
+  }
+
+  @Test
+  void testExec_part2_case2() {
+    var code = Lists.newArrayList(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(1)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(8).iterator()), is(List.of(0)));
+  }
+
+  @Test
+  void testExec_part2_case3() {
+    var code = Lists.newArrayList(3, 3, 1108, -1, 8, 3, 4, 3, 99);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(0)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(8).iterator()), is(List.of(1)));
+  }
+
+  @Test
+  void testExec_part2_case4() {
+    var code = Lists.newArrayList(3, 3, 1107, -1, 8, 3, 4, 3, 99);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(1)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(8).iterator()), is(List.of(0)));
+  }
+
+  @Test
+  void testExec_part2_case5() {
+    var code = Lists.newArrayList(3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(0)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(2).iterator()), is(List.of(1)));
+  }
+
+  @Test
+  void testExec_part2_case6() {
+    var code = Lists.newArrayList(3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(0)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(2).iterator()), is(List.of(1)));
+  }
+
+  @Test
+  void testExec_part2_case7() {
+    var code =
+        Lists.newArrayList(
+            3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0,
+            0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4,
+            20, 1105, 1, 46, 98, 99);
+    assertThat(computer.exec(new ArrayList<>(code), List.of(0).iterator()), is(List.of(999)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(8).iterator()), is(List.of(1000)));
+    assertThat(computer.exec(new ArrayList<>(code), List.of(9).iterator()), is(List.of(1001)));
+  }
+
+  @Test
+  void testPart2() {
+    var localCode = new ArrayList<>(code);
+    var output = computer.exec(localCode, List.of(5).iterator());
+    assertTrue(output.subList(0, output.size() - 1).stream().allMatch(code -> code.equals(0)));
+    assertThat(output.get(output.size() - 1), is(5525561));
   }
 }
